c89ce1150fd43466ee431d8d433293c8a6c740e3
Md Hefzul Bari <ratul14april@gmail.com>
2021-11-02 12:49:40 -0600

    Using isEven funtion to identify odd and even numbers

---
+++ even.js
@@ -0,0 +1,17 @@
+const isEven = function(num){
+if(num % 2 === 0){
+console.log(num + " is an even number!");
+} else if(num % 2 !== 0){
+console.log(num + " in an odd number!");
+}
+}
+
+//const tenIsEven = isEven(10);
+//const elevenIsEven = isEven(11);
+
+//console.log(tenIsEven);
+//console.log(elevenIsEven);
+
+isEven(10);
+
+isEven(11);
