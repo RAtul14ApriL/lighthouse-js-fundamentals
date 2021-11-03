2acc78d08a6a2451fad9ffbbef6ed1ef5b2fc746
Md Hefzul Bari <ratul14april@gmail.com>
2021-11-03 01:57:55 -0600

    Using arrays to arrange ice cream flavours

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+iceCreamFlavours.push("Root beer");
+
+let lastFlavour = iceCreamFlavours.length - 1;
+
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[lastFlavour]);
+console.log(iceCreamFlavours.length);
